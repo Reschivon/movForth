c068d1610671c8e9b14e04b2da7bd866f6f23a06
Reschivon <xchenbox@gmail.com>
2021-08-17 00:39:29 -0400

    update boot.fs with better explanations

--- boot.fs
+++ boot.fs
@@ -1,41 +1,38 @@
 
 \ Compilation steps:
 \
-\ First, this file is interpreted similar to a typical Forth program. This way,
+\ First, this file is interpreted as a typical Forth program. This way,
 \ user-defined compiling actions actually do execute during compilation time
 \
-\ Next, in main.cpp, we instruct the compiler to compile a certain word in the
-\ dictionary. This is important, becasue some lower level words manipulate
-\ interpreter state and cannot be compiled as as standalone word.
-\
+\ Next, in main.cpp, we instruct the compiler to compile a chosen word in the
+\ dictionary ("test" in this case)
 \
 \        Dependency Tree:
-\ Then, the compiler builds a dependency tree for the selected word. The word is
-\ the root node and all of its offspring are derived from its definition.
-\ This happens recursively until a primitive, which has no dependecies, is reached
+\ Then, the compiler builds a dependency tree for the selected word. The word forms
+\ the root node and all of its offspring nodes are determined from its definition.
+\ This happens recursively until a primitive word, which has no dependecies, is reached
 \
-\ As this tree is built, the compiler collapses data stored in the definition
+\ As this tree is built, the compiler collapses number data stored in the definition
 \ (like the number that proceeds a LITERAL) into the word that owns it
 \
 \ When all offspring of a certain node have been built, the compiler searches
-\ the node(or word)'s definition for BRANCH and BRANCHIF. The compiler uses this
+\ the node/word's definition for BRANCH and BRANCHIF. The compiler uses this
 \ information to make a control flow graph. Each word/node has its own control
 \ flow graph
 \
-\ A sequence of words that doesn't include BRANCH or BRANCHIF
+\ A sequence of words that don't include BRANCH or BRANCHIF
 \ is a basic block. This is the same defintion that LLVM uses.
 \
 \
 \        Stack Graph:
-\ This compiler compiles forth to LLVM IR. Then LLVM does the hard work of
-\ optimizing and writing assembly. But LLVM uses SSA, which is a register-based
-\ format, and Forth is a stack-based. So a stack graph is needed to symbolically
+\ This compiler compiles forth to LLVM IR. But LLVM uses SSA, which is a register-based
+\ format, and Forth is a stack-based. So a stack graph is needed to
 \ link the output of one word to the input of another.
 \
 \ Imagine a sequence of words, each with as many input nodes as it
-\ takes from the stack and as many output nodes as it pushes to the
-\ stack. The stack graph algorithm creates an edge between every pair of input\output
-\ nodes and assigns a name to the edge. An edge is a register.
+\ takes elements from the stack and as many output nodes as elements that it pushes to the
+\ stack. The stack graph algorithm creates an edge between every pair of input/output
+\ nodes and assigns a name to the edge. An edge becomes a register in IR.
 \
 \ The stack graph is printed out on the console during compilation.
 \
@@ -46,16 +43,19 @@
 \
 \        Stack Graph with Control Flow
 \ The compiler supports not just structured control flow but any CFG
-\ that can be represented by BRANCH and BRANCHIF. Even if two basic blocks
-\ merge into the same final BB but push different register indexes,
-\ the registers that cross control flow edges are guaranteed to be the same
+\ that can be represented by BRANCH and BRANCHIF. If two basic blocks
+\ merge into the same final BB, they will not push differently named registers,
+\ In other words, the registers that cross control flow edges are guaranteed to be the same
 \ for all merging basic blocks.
 
 \ (I made a very elegant solution to this here. It piggybacks on the control
-\ flow-less stack graph DFS without needing passes of its own)
+\ flow DFS pass so it doesn't need its own pass)
 
-\ This assumes that BBs that merge together push the same number of
-\ stack elements. Otherwise, this would be a very, very hard problem.
+
+\ BBs that merge together *must* push the same number of
+\ stack elements. Making this assumption makes the code much easier to write.
+
+
 
 
 : ['] immediate ' ;
@@ -110,6 +110,9 @@
 : repeatif immediate
     <jumpif ;
 
+\ the word to be compiled. "test" is specified in main.cpp
+\ control flow words (BRANCH and BRANCHIF) compiled by IF, ELSE, and THEN
+\ are supported by the compiler
 : test 1 if 69 69 else 420 420 then ;
 
 see
